40dd1853bad1213923636fa9510aa9a11e8e87ea
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-07-28 09:16:33 +0400

    create project structur

---
+++ miniBankSystemOOP/Codeline_HealthCareCenter_OOP/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.Models
+{
+    class Patient
+    {
+    }
+}
---
+++ miniBankSystemOOP/Codeline_HealthCareCenter_OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Codeline_HealthCareCenter_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
